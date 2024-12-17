5706f720978843fa1680f8232f06e3c6750c4558
Marco <marcospies2000@gmail.com>
2024-12-17 11:06:26 +0100

    Black backround

--- r.ps1
+++ r.ps1
@@ -1,1 +1,26 @@
-Start-Process wordpad
+#Changes Background  
+$url = "https://www.solidbackgrounds.com/images/3840x2160/3840x2160-black-solid-color-background.jpg"
+
+
+Invoke-WebRequest $url -OutFile C:\temp\black.jpg
+
+
+$setwallpapersrc = @"
+using System.Runtime.InteropServices;
+
+public class Wallpaper
+{
+  public const int SetDesktopWallpaper = 20;
+  public const int UpdateIniFile = 0x01;
+  public const int SendWinIniChange = 0x02;
+  [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+  private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+  public static void SetWallpaper(string path)
+  {
+    SystemParametersInfo(SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange);
+  }
+}
+"@
+Add-Type -TypeDefinition $setwallpapersrc
+
+[Wallpaper]::SetWallpaper("C:\temp\black.jpg")
